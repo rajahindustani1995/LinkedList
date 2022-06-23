7ffab94bd7428824ef7c4b8915b6919044c339d8
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-23 14:39:09 +0530

    [Rajesh] Added : Linked List

---
+++ DataStructure/DataStructure/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked List is empty");
+            else
+                Console.WriteLine("Linked List");
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.data != null)
+                    Console.Write("->");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace DataStructure
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+            //linkedList.LinkedList(node1);
+            //linkedList.LinkedList(node2);
+            //linkedList.LinkedList(node3);
+            linkedList.Display();
+        }
+    }
+}
